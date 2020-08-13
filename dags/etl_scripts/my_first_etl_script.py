7c523fc3e49e606f40ab5bcd9c6b6e910d2f4475
michellima28 <michelcorreia@outlook.com>
2020-08-13 16:24:02 -0300

    dags folder

---
+++ dags/etl_scripts/my_first_etl_script.py
@@ -0,0 +1,1 @@
+print('Executed code from my Python ETL!')
---
+++ dags/my_first_dag.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.bash_operator import BashOperator
+
+default_args = {
+   'owner': 'michel_lima',
+   'depends_on_past': False,
+   'start_date': datetime(2019, 1, 1),
+   'retries': 0,
+   }
+
+with DAG(
+   'my-first-dag',
+   schedule_interval=timedelta(minutes=1),
+   catchup=False,
+   default_args=default_args
+   ) as dag:
+
+   t1 = BashOperator(
+   task_id='first_etl',
+   bash_command="""
+   cd /root/airflow/dags/etl_scripts/
+   python3 my_first_etl_script.py
+   """)
+
+   t2 = BashOperator(
+   task_id='second_etl',
+   bash_command="""
+   cd /root/airflow/dags/etl_scripts/
+   python3 my_second_etl_script.py
+   """)
+
+t1 >> t2
+      
